4ac0dfd55b5c38a0de364b7ba9f23a72e61fa6a6
Jenny <47075939+dontbehasty@users.noreply.github.com>
2020-05-10 20:18:50 +0100

    Add files via upload

---
+++ test/list_users2.js
@@ -0,0 +1,23 @@
+const supertest = require('supertest');
+
+var request = require('supertest')("https://reqres.in");
+var get_user = "/api/users/2";
+var assert = require('assert');
+
+describe('Get Single User', function() {
+    it('checks that the response was OK', function(done) {
+        request
+            .get(get_user)
+            .expect(200, done);
+
+    });
+
+    it('checks email is correct', function(done) {
+        request
+            .get(get_user)
+            .end(function(err, result) {
+                assert.equal(result.body.data.email, 'janet.weaver@reqres.in');
+                done();       
+            }); 
+    });
+});
