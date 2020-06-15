dc884bea665b94ab34bfd63d2e6e0d7dfa6d1cb1
Jenny <47075939+dontbehasty@users.noreply.github.com>
2020-06-15 19:39:03 +0100

    Remove first name test

--- test/list_users.js
+++ test/list_users.js
@@ -31,7 +31,6 @@ describe('First Request to Get Users',()=>{
     it('gets the id from the array',async()=>{
          for(let i=0;i<res.body.data.length;i++){
             await(console.log(res.body.data[i].id));
-            await (console.log(res.body.data[i].first_name));
          }
     });
 
